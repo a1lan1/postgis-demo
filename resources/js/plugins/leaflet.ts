c55fe24301391251d33a7b6880c7a28af54efe61
Aleksandr Smirnov <genesboard@gmail.com>
2025-10-24 20:26:28 +0300

    add vue-leaflet map

---
+++ resources/js/plugins/leaflet.ts
@@ -0,0 +1,16 @@
+import { Icon, type IconOptions } from 'leaflet'
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png'
+import markerIcon from 'leaflet/dist/images/marker-icon.png'
+import markerShadow from 'leaflet/dist/images/marker-shadow.png'
+import 'leaflet/dist/leaflet.css'
+
+Object.assign(Icon.Default.prototype.options, {
+  iconUrl: markerIcon,
+  iconRetinaUrl: markerIcon2x,
+  shadowUrl: markerShadow,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  tooltipAnchor: [16, -28],
+  shadowSize: [41, 41]
+} as IconOptions)
