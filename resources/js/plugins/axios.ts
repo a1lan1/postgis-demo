1457aed9e02264f53709aebd12551ec764b5c1bf
Aleksandr Smirnov <genesboard@gmail.com>
2025-10-25 13:08:59 +0300

    add axios plugin with pinia context

---
+++ resources/js/plugins/axios.ts
@@ -0,0 +1,19 @@
+import axios, { type AxiosInstance } from 'axios'
+import { type PiniaPluginContext } from 'pinia'
+import { type App } from 'vue'
+
+export function piniaAxiosPlugin({ store, app }: PiniaPluginContext) {
+  store.$axios = app.config.globalProperties.$axios
+}
+
+export default {
+  install(app: App) {
+    app.config.globalProperties.$axios = axios.create({
+      withCredentials: true,
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Requested-With': 'XMLHttpRequest'
+      }
+    }) as AxiosInstance
+  }
+}
--- resources/js/plugins/index.ts
+++ resources/js/plugins/index.ts
@@ -1,7 +1,10 @@
 import './leaflet'
 import pinia from './pinia'
+import axios from './axios'
 import type { App } from 'vue'
 
 export function registerPlugins(app: App) {
-  app.use(pinia)
+  app
+    .use(pinia)
+    .use(axios)
 }
