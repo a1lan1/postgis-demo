c932feb077488ff003e03214d9152c02fc96aef1
Aleksandr Smirnov <genesboard@gmail.com>
2025-10-25 14:12:35 +0300

    add autocomplete ui component

---
+++ resources/js/components/ui/autocomplete/Autocomplete.vue
@@ -0,0 +1,87 @@
+<script setup lang="ts">
+import { ref, watch, useAttrs } from 'vue'
+import { useDebounceFn } from '@vueuse/core'
+import Input from '@/components/ui/input/Input.vue'
+
+interface AutocompleteItem {
+  id: number | string
+  name: string
+}
+
+const props = withDefaults(
+  defineProps<{
+    search: (query: string) => Promise<AutocompleteItem[]>
+    delay?: number
+    minLength?: number
+  }>(),
+  {
+    delay: 300,
+    minLength: 2,
+  },
+)
+
+const searchTerm = ref('')
+const results = ref<AutocompleteItem[]>([])
+const isLoading = ref(false)
+const isDropdownOpen = ref(false)
+
+const searchHandler = useDebounceFn(async (query: string) => {
+  if (query.length < props.minLength) {
+    results.value = []
+    isDropdownOpen.value = false
+    return
+  }
+
+  isLoading.value = true
+  isDropdownOpen.value = true
+  try {
+    results.value = await props.search(query)
+  }
+  catch (error) {
+    console.error('Autocomplete search error:', error)
+    results.value = []
+  }
+  finally {
+    isLoading.value = false
+  }
+}, props.delay)
+
+watch(searchTerm, (newQuery) => {
+  searchHandler(newQuery)
+})
+
+function selectItem(item: AutocompleteItem) {
+  searchTerm.value = item.name
+  isDropdownOpen.value = false
+}
+</script>
+
+<template>
+  <div class="relative">
+    <Input
+      v-model="searchTerm"
+      v-bind="useAttrs()"
+      @focus="isDropdownOpen = results.length > 0"
+      @blur="setTimeout(() => (isDropdownOpen = false), 200)"
+    />
+
+    <div
+      v-if="isDropdownOpen && (isLoading || results.length > 0)"
+      class="absolute z-10 mt-1 w-full rounded-md border border-gray-300 bg-white shadow-lg text-black"
+    >
+      <div v-if="isLoading" class="px-4 py-2">
+        Loading...
+      </div>
+      <ul v-else>
+        <li
+          v-for="item in results"
+          :key="item.id"
+          class="cursor-pointer px-4 py-2 hover:bg-gray-100 "
+          @mousedown="selectItem(item)"
+        >
+          {{ item.name }}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
