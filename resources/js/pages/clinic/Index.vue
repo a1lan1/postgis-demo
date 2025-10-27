33963919865b3ca063eb66f868be63c78e35b225
Aleksandr Smirnov <genesboard@gmail.com>
2025-10-27 15:20:28 +0300

    add clinics map page

---
+++ resources/js/pages/clinic/Index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { Head } from '@inertiajs/vue3'
+import ClinicsMap from '@/components/clinic/ClinicsMap.vue'
+</script>
+
+<template>
+  <Head title="Clinics" />
+
+  <main class="flex w-full">
+    <ClinicsMap />
+  </main>
+</template>
