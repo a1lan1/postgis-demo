a5d7880189df23c78d0fde1eb5df46049b635a1a
Aleksandr Smirnov <genesboard@gmail.com>
2025-10-28 05:32:21 +0300

    add pest config

--- tests/Pest.php
+++ tests/Pest.php
@@ -12,8 +12,19 @@
 */
 
 pest()->extend(Tests\TestCase::class)
- // ->use(Illuminate\Foundation\Testing\RefreshDatabase::class)
+    ->use(Illuminate\Foundation\Testing\RefreshDatabase::class)
     ->in('Feature');
+
+pest()->extend(Tests\TestCase::class)
+    ->use(Illuminate\Foundation\Testing\RefreshDatabase::class)
+    ->in('Unit');
+
+pest()->extend(Tests\TestCase::class)
+    ->use(Illuminate\Foundation\Testing\RefreshDatabase::class)
+    ->group('browser')
+    ->in('Browser');
+
+pest()->browser()->inChrome();
 
 /*
 |--------------------------------------------------------------------------
